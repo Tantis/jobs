6b975375079b32c72af8ca2cef89ca446ee78a5d
liuy <liuy@huitouche.com>
2017-02-10 11:01:25 +0800

    更新：
    修复网页打开报错

--- server/static/js/inspinia.js
+++ server/static/js/inspinia.js
@@ -6,7 +6,7 @@
  */
 
 
-$(document).ready(function () {
+$(document).ready(function() {
 
 
     // Add body-small class if window less than 768px
@@ -20,39 +20,39 @@ $(document).ready(function () {
     $('#side-menu').metisMenu();
 
     // Collapse ibox function
-    $('.collapse-link').click(function () {
+    $('.collapse-link').click(function() {
         var ibox = $(this).closest('div.ibox');
         var button = $(this).find('i');
         var content = ibox.find('div.ibox-content');
         content.slideToggle(200);
         button.toggleClass('fa-chevron-up').toggleClass('fa-chevron-down');
         ibox.toggleClass('').toggleClass('border-bottom');
-        setTimeout(function () {
+        setTimeout(function() {
             ibox.resize();
             ibox.find('[id^=map-]').resize();
         }, 50);
     });
 
     // Close ibox function
-    $('.close-link').click(function () {
+    $('.close-link').click(function() {
         var content = $(this).closest('div.ibox');
         content.remove();
     });
 
     // Fullscreen ibox function
-    $('.fullscreen-link').click(function () {
+    $('.fullscreen-link').click(function() {
         var ibox = $(this).closest('div.ibox');
         var button = $(this).find('i');
         $('body').toggleClass('fullscreen-ibox-mode');
         button.toggleClass('fa-expand').toggleClass('fa-compress');
         ibox.toggleClass('fullscreen');
-        setTimeout(function () {
+        setTimeout(function() {
             $(window).trigger('resize');
         }, 100);
     });
 
     // Close menu in canvas mode
-    $('.close-canvas-menu').click(function () {
+    $('.close-canvas-menu').click(function() {
         $("body").toggleClass("mini-navbar");
         SmoothlyMenu();
     });
@@ -64,7 +64,7 @@ $(document).ready(function () {
     });
 
     // Open close right sidebar
-    $('.right-sidebar-toggle').click(function () {
+    $('.right-sidebar-toggle').click(function() {
         $('#right-sidebar').toggleClass('sidebar-open');
     });
 
@@ -76,7 +76,7 @@ $(document).ready(function () {
     });
 
     // Open close small chat
-    $('.open-small-chat').click(function () {
+    $('.open-small-chat').click(function() {
         $(this).children().toggleClass('fa-comments').toggleClass('fa-remove');
         $('.small-chat-box').toggleClass('active');
     });
@@ -88,7 +88,7 @@ $(document).ready(function () {
     });
 
     // Small todo handler
-    $('.check-link').click(function () {
+    $('.check-link').click(function() {
         var button = $(this).find('i');
         var label = $(this).next('span');
         button.toggleClass('fa-check-square').toggleClass('fa-square-o');
@@ -98,13 +98,13 @@ $(document).ready(function () {
 
     // Append config box / Only for demo purpose
     // Uncomment on server mode to enable XHR calls
-    $.get("skin-config.html", function (data) {
-        if (!$('body').hasClass('no-skin-config'))
-            $('body').append(data);
-    });
+    //$.get("skin-config.html", function (data) {
+    //    if (!$('body').hasClass('no-skin-config'))
+    //        $('body').append(data);
+    //});
 
     // Minimalize menu
-    $('.navbar-minimalize').click(function () {
+    $('.navbar-minimalize').click(function() {
         $("body").toggleClass("mini-navbar");
         SmoothlyMenu();
 
@@ -149,7 +149,7 @@ $(document).ready(function () {
     fix_height();
 
     // Fixed Sidebar
-    $(window).bind("load", function () {
+    $(window).bind("load", function() {
         if ($("body").hasClass('fixed-sidebar')) {
             $('.sidebar-collapse').slimScroll({
                 height: '100%',
@@ -159,7 +159,7 @@ $(document).ready(function () {
     });
 
     // Move right sidebar top after scroll
-    $(window).scroll(function () {
+    $(window).scroll(function() {
         if ($(window).scrollTop() > 0 && !$('body').hasClass('fixed-nav')) {
             $('#right-sidebar').addClass('sidebar-top');
         } else {
@@ -167,7 +167,7 @@ $(document).ready(function () {
         }
     });
 
-    $(window).bind("load resize scroll", function () {
+    $(window).bind("load resize scroll", function() {
         if (!$("body").hasClass('body-small')) {
             fix_height();
         }
@@ -184,7 +184,7 @@ $(document).ready(function () {
 
 
 // Minimalize menu when screen is less than 768px
-$(window).bind("resize", function () {
+$(window).bind("resize", function() {
     if ($(this).width() < 769) {
         $('body').addClass('body-small')
     } else {
@@ -194,7 +194,7 @@ $(window).bind("resize", function () {
 
 // Local Storage functions
 // Set proper body class and plugins based on user configuration
-$(document).ready(function () {
+$(document).ready(function() {
     if (localStorageSupport) {
 
         var collapse = localStorage.getItem("collapse_menu");
@@ -250,12 +250,12 @@ function localStorageSupport() {
 function animationHover(element, animation) {
     element = $(element);
     element.hover(
-        function () {
+        function() {
             element.addClass('animated ' + animation);
         },
-        function () {
+        function() {
             //wait for animation to finish before removing classes
-            window.setTimeout(function () {
+            window.setTimeout(function() {
                 element.removeClass('animated ' + animation);
             }, 2000);
         });
@@ -267,13 +267,13 @@ function SmoothlyMenu() {
         $('#side-menu').hide();
         // For smoothly turn on menu
         setTimeout(
-            function () {
+            function() {
                 $('#side-menu').fadeIn(400);
             }, 200);
     } else if ($('body').hasClass('fixed-sidebar')) {
         $('#side-menu').hide();
         setTimeout(
-            function () {
+            function() {
                 $('#side-menu').fadeIn(400);
             }, 100);
     } else {
@@ -287,8 +287,7 @@ function WinMove() {
     var element = "[class*=col]";
     var handle = ".ibox-title";
     var connect = "[class*=col]";
-    $(element).sortable(
-        {
+    $(element).sortable({
             handle: handle,
             connectWith: connect,
             tolerance: 'pointer',
@@ -297,5 +296,3 @@ function WinMove() {
         })
         .disableSelection();
 }
-
-
