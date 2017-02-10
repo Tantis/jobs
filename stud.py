c103867a80d1d4900cd3cf3f4c00addff45f064e
liuy <liuy@huitouche.com>
2017-02-10 17:03:43 +0800

    更新文件监控

--- stud.py
+++ stud.py
@@ -6,17 +6,39 @@
 
 import requests
 import json
+import time
+import os
+
+SEEK = 0
+if os.path.exists('file_status.pid'):
+    with open('file_status.pid', 'r') as seek:
+        SEEK = int(seek.read())
 
 if __name__ == "__main__":
 
-    with open('access.log', 'r') as f:
-        pline = f.readlines()
-    url = "https://api.map.baidu.com/location/ip?ak=Ay7G6RvYQMQxBGujnpLdxb93&coor=bd09ll&ip={0}"
-    pox = []
-    for item in pline:
-        #print(item.split('ip:')[1])
-        r = requests.get(url.format(item.split('ip:')[1].strip()))
-        pox.append({item.split('ip:')[1].strip(): r.json()})
+    while True:
+        with open('access.log', 'r') as f:
 
-    with open('addr.log', 'a') as f:
-        f.write(json.dumps(pox))
+            pline = f.readlines()
+
+
+        seek = len(pline)
+
+
+        if seek == SEEK:
+            time.sleep(30)
+            continue
+
+        url = "https://api.map.baidu.com/location/ip?ak=Ay7G6RvYQMQxBGujnpLdxb93&coor=bd09ll&ip={0}"
+        pox = []
+        for item in pline[SEEK:]:
+            # print(item.split('ip:')[1])
+            r = requests.get(url.format(item.split('ip:')[1].strip()))
+            pox.append({item.split('ip:')[1].strip(): r.json()})
+
+        with open('addr.log', 'a') as f:
+            f.write(json.dumps(pox))
+
+        with open('file_status.pid', 'w') as sek:
+            sek.write(str(seek))
+        SEEK = seek
