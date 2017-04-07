c5f0456cad33cc6a18b4ffda1ae56e98d893b992
luer_liuyu <showmove@qq.com>
2017-04-08 02:50:40 +0800

    UP

--- app.py
+++ app.py
@@ -1,10 +1,10 @@
-import eventlet
-from eventlet import wsgi
+#import eventlet
+#from eventlet import wsgi
 
 from server import app
 
 if __name__ == '__main__':
 
-    #app.debug = True
-    #app.run(host='0.0.0.0', port=84)
-    wsgi.server(eventlet.listen(('', 84)), app)
+    app.debug = True
+    app.run(host='0.0.0.0', port=84)
+    #wsgi.server(eventlet.listen(('', 84)), app)
